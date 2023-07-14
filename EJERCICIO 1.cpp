2ee042c71d9de6483d885943c6f3ca1541f3e773
wjosorio <139577093+wjosorio@users.noreply.github.com>
2023-07-14 14:09:20 -0500

    Add files via upload

---
+++ EJERCICIO 1.cpp
@@ -0,0 +1,62 @@
+//CONSTRUCCIÓN DE LA PLANTILLA PARA EL PROGRAMA MENU FUNCIONES CON
+//SWITCH CASE
+
+
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    int opcion;
+    bool repetir = true;
+    
+    do {
+        system("cls");
+        
+        // Texto del menú que se verá cada vez
+        cout << "\n\nMenu de Opciones" << endl;
+        cout << "1. Opcion 1" << endl;
+        cout << "2. Opcion 2" << endl;
+        cout << "3. Opcion 3" << endl;
+        cout << "4. Opcion 4" << endl;
+        cout << "0. SALIR" << endl;
+        
+        cout << "\nIngrese una opcion: ";
+        cin >> opcion;
+        
+        switch (opcion) {
+            case 1:
+                // Lista de instrucciones de la opción 1                
+                
+                system("pause>nul"); // Pausa
+                break;
+                
+            case 2:
+                // Lista de instrucciones de la opción 2                
+                
+                system("pause>nul"); // Pausa
+                break;
+                
+            case 3:
+                // Lista de instrucciones de la opción 3                
+                
+                system("pause>nul"); // Pausa            
+                break;
+                
+            case 4:
+                // Lista de instrucciones de la opción 4                
+                
+                system("pause>nul"); // Pausa                
+                break;
+            
+            case 0:
+            	repetir = false;
+            	break;
+        }        
+    } while (repetir);
+	 
+    return 0;
+}
